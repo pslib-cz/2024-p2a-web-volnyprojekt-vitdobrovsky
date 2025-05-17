2e3e25f70e0d35c5d354b23b8595a6be61ee4667
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-05-17 20:03:39 +0200

    swiper js

---
+++ src/scripts/swiper.js
@@ -0,0 +1,21 @@
+import Swiper from "swiper";
+import { Autoplay, Pagination } from "swiper/modules";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Autoplay, Pagination],
+
+  direction: "horizontal",
+  loop: true,
+  spaceBetween: 32,
+  touchEventsTarget: "container",
+  simulateTouch: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  autoplay: {
+    delay: 8000,
+    disableOnInteraction: false,
+  },
+});
