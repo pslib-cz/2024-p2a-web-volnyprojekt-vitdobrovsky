6ce22837fe6cd4f0a241e6a3ac4ef649eac18803
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-05-18 16:59:26 +0200

    close menu

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -3,5 +3,9 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger--active");
 });
 
-
-
+// close the hamburger menu when clicking outside of it
+document.addEventListener("click", (event) => {
+  if (!hamburger.contains(event.target)) {
+    hamburger.classList.remove("hamburger--active");
+  }
+});
