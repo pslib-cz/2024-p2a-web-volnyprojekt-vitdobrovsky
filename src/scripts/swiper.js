b1f345acefcc8d55156e4163e001ec96ae3024ac
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-05-19 14:04:19 +0200

    swiper update

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,17 +1,22 @@
 import Swiper from "swiper";
-import { Autoplay, Pagination } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const swiper = new Swiper(".swiper", {
-  modules: [Autoplay, Pagination],
+  modules: [Autoplay, Pagination, Navigation],
 
   direction: "horizontal",
   loop: true,
-  spaceBetween: 48,
+  spaceBetween: 64,
   touchEventsTarget: "container",
   simulateTouch: true,
 
   pagination: {
     el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   autoplay: {
